a404e02736a27eb9bf03c0c0a8ceb9e3243795a9
ViniciusNeivas <viniciusneivas@outlook.com.br>
2023-05-29 11:30:26 -0300

    Inicialização do projeto, adição de perguntas, configurações para ler json, imagem do logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuizzComponent } from './components/quizz/quizz.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizzComponent,
+    HomeComponent,
   ],
   imports: [
+    RouterModule,
     BrowserModule
   ],
   providers: [],
